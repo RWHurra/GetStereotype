d19c15881810eda84002b1d983909da3a983bcef
RWHurra <robert.hurra@arway.se>
2016-09-20 14:42:26 +0200

    Fetch stereotype as string
    This is a method to fetch an elements stereotype as a string.

---
+++ GetStereotype/AddIn.cs
@@ -0,0 +1,32 @@
+﻿using EA;
+using System;
+using System.Runtime.InteropServices;
+
+namespace GetStereotype
+{
+    [ComVisible(true)]
+    public class AddIn
+    {
+
+        //Connect to Sparx repository
+        public string EA_Connect(EA.Repository rep)
+        {
+            return "GetStereotype.AddIn - connected";
+        }
+
+        //Create class to fetch steroetype as string
+        public string GetValueForStereotype(EA.Repository rep, string eaGuid, object stereotype)
+        {
+            EA.Element element = rep.GetElementByGuid(eaGuid);
+            string ret = element.Stereotype;
+            return ret;
+        }
+
+        //Disconnect from Sparx repository
+        public void EA_Disconnect()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
